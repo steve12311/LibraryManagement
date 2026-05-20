29dc464713085a78a12a16f6737eb11fd0a701a8
steve12311 <52844861+steve12311@users.noreply.github.com>
2026-05-20 11:51:45 +0800

    fix(reservation): add @TableId and @ValidField annotations

--- managementsystem-system/src/main/java/org/dwtech/system/model/entity/ReservationPO.java
+++ managementsystem-system/src/main/java/org/dwtech/system/model/entity/ReservationPO.java
@@ -2,6 +2,7 @@ package org.dwtech.system.model.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.util.Date;
 @Data
 @TableName("lib_reservation")
 public class ReservationPO {
+    @TableId
     private String id;
     private String isbn;
     private String bookName;
--- managementsystem-system/src/main/java/org/dwtech/system/model/query/ReservationPageQuery.java
+++ managementsystem-system/src/main/java/org/dwtech/system/model/query/ReservationPageQuery.java
@@ -2,11 +2,13 @@ package org.dwtech.system.model.query;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.dwtech.common.annontation.ValidField;
 import org.dwtech.common.base.BasePageQuery;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ReservationPageQuery extends BasePageQuery {
+    @ValidField(allowedValues = {"username", "isbn", "status"})
     private String field;
     private String keyword;
 }
